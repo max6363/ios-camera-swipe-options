04ed9170bdd4983e9acf0bb3331ff49dd10b3ced
Minhaz Panara <minhaz.panara@gmail.com>
2017-09-07 21:16:14 +0530

    Initial Commit

---
+++ ios-camera-swipe-options/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-camera-swipe-options
+//
+//  Created by Minhaz on 07/09/17.
+//  Copyright © 2017 aub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios-camera-swipe-optionsTests/ios_camera_swipe_optionsTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_camera_swipe_optionsTests.swift
+//  ios-camera-swipe-optionsTests
+//
+//  Created by Minhaz on 07/09/17.
+//  Copyright © 2017 aub. All rights reserved.
+//
+
+import XCTest
+@testable import ios_camera_swipe_options
+
+class ios_camera_swipe_optionsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
